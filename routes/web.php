3f0659dce5beedabf293aaa89a059997c4dd9eef
ISHIMWE TETA Liana <tetaliana287@gmail.com>
2026-02-05 12:58:22 +0200

    personal access token

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route used by the password reset notification to generate reset URLs.
+Route::get('/reset-password/{token}', function (string $token) {
+    return 'Password reset placeholder. Token: '.$token;
+})->name('password.reset');
